0e6dd41ce1738521e51e3b13b1d4855256d80476
valmarsilva <valmar-ssilva@hotmail.com>
2021-01-10 18:38:56 -0300

    Add routes and StepsHeader

--- sds2-aulas/front-web/src/App.tsx
+++ sds2-aulas/front-web/src/App.tsx
@@ -1,17 +1,16 @@
 
 
 import './App.css';
-import Home from './Home';
 import Navbar from './Navbar';
-
+import Routes from './Routes';
 function App() {
   return (
     
-    <>
-     <Navbar /> 
-     <Home />
+    
+    
+     <Routes />
      
-    </>
+    
   );
 }
 
---
+++ sds2-aulas/front-web/src/Orders/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import StepsHeader from './StepsHeader';
+function Orders(){
+    return (
+
+       <div className= "orders-conteiner">
+     <StepsHeader />
+       </div>
+    )
+}
+export default Orders;
