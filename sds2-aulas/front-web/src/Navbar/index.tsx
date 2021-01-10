0b8e01e76b91cf4e3fd12e1cd1e05d91da5b40c8
valmarsilva <valmar-ssilva@hotmail.com>
2021-01-10 11:57:57 -0300

    Add Navbar

--- sds2-aulas/front-web/src/Navbar/index.tsx
+++ sds2-aulas/front-web/src/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import './styles.css';
-
+import {ReactComponent as Logo} from './logo.svg';
 function Navbar(){
     return (
 
         <nav  className="main-navbar">
 
 
-
+<Logo />
+<a href="home" className="logo-text">DS Delivery</a>
         </nav>
     )
 }
