95ce85a2beb6e6fe574b49428ba34482d681175e
valmarsilva <valmar-ssilva@hotmail.com>
2021-01-10 14:14:42 -0300

    Add Home page

--- sds2-aulas/front-web/src/App.tsx
+++ sds2-aulas/front-web/src/App.tsx
@@ -1,12 +1,17 @@
 
+
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div>
+    
+    <>
      <Navbar /> 
-    </div>
+     <Home />
+     
+    </>
   );
 }
 
