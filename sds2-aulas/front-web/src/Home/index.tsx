3738cac263c576499a81f9358588cc8240231565
valmarsilva <valmar-ssilva@hotmail.com>
2021-01-10 15:38:17 -0300

    Add Footer

--- sds2-aulas/front-web/src/Home/index.tsx
+++ sds2-aulas/front-web/src/Home/index.tsx
@@ -1,35 +1,47 @@
 import './styles.css';
-import  {     ReactComponent as MainImage }from './main.svg';
+import { ReactComponent as MainImage } from './main.svg';
+import Footer from '../Footer/indesx';
 function Home() {
-    return(
+    return (
+        <>
 
-<div className="home-conteiner">
-    <div className="home-content"></div>
-<div className="home-actions">
+            <div className="home-conteiner">
+                <div className="home-content"></div>
+                <div className="home-actions">
 
-    <h1 className="home -titlle">
-    Faça seu pedido<br /> que entregamos<br />pra você!!!
-    </h1>
-    <h3 className="home-subtitle">
-        
-    Escolha o seu pedido e em poucos minutos<br/>
+                    <h1 className="home -titlle">
+                        Faça seu pedido<br /> que entregamos<br />pra você!!!
+                     </h1>
+                    <h3 className="home-subtitle">
+
+                        Escolha o seu pedido e em poucos minutos<br />
      levaremoss na sua porta
     </h3>
-    <a href="orders" className="home-btn-order">
-        FAZER PEDIDO
+                    <a href="orders" className="home-btn-order">
+                        FAZER PEDIDO
 
     </a>
-</div>
-<div className="home-image">
-    <MainImage />
+                </div>
+                <div className="home-image">
+                    <MainImage />
+
+
+                </div>
+                </div>
+                <Footer />
+
+   </>
+  )
+        
+
+
+
+
+
+
+
+
     
-
-</div>
-
-</div>
-
-
-    )
     
 }
 export default Home;
