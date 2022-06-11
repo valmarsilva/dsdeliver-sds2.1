480d6eee07efee7b12f139baea3dbde794a1c474
valmar Silva <valmar-ssilva@hotmail.com>
2022-06-11 01:29:05 -0300

    Add Footer

--- frontend/front-web/src/Home/index.tsx
+++ frontend/front-web/src/Home/index.tsx
@@ -4,30 +4,29 @@ import Footer from'../Footer';
 
 function Home() {
   return (
-
-
- <div className="home-container">
-      <div className="home-content">
-       <div className="home-actions">
-         <h1 className="home-title">
-          Faça seu pedido <br /> que entregamos  <br /> para você!!!
-        </h1>
-        <h3  className="home-sbtitle">
-          Escolha seu pedido e em poucos minutos <br />
-           entregaremos na casa
-        </h3>
-        <a  href="orders"className="home-btn-order">
-          FAZER PEDIDO
-        </a>
-         </div>
-         <div  className="home-image">
-          <MainImage />
-         </div>
-         </div>
-         </div>
-
-
-    );
+    <>
+      <div className="home-container">
+        <div className="home-content">
+          <div className="home-actions">
+            <h1 className="home-title">
+              Faça seu pedido <br /> que entregamos <br /> para você!!!
+            </h1>
+            <h3 className="home-sbtitle">
+              Escolha seu pedido e em poucos minutos <br />
+              entregaremos na casa
+            </h3>
+            <a href="orders" className="home-btn-order">
+              FAZER PEDIDO
+            </a>
+          </div>
+          <div className="home-image">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
   }
 
 export default Home;
