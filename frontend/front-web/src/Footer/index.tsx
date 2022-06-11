6b1985813398f86ae8518216a53a8876eb57d9aa
valmar Silva <valmar-ssilva@hotmail.com>
2022-06-11 00:51:22 -0300

    Add Home page

---
+++ frontend/front-web/src/Footer/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+import {ReactComponent as Logo}from './logo.svg';
+
+function Footer()
+ {
+   return (
+     <footer className="main-footer">
+       App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+     </footer>
+   );
+ }
+ export default Footer;
