0a9e378ded28e94d8b8d71e1dcf4f889db1a778b
valmar Silva <valmar-ssilva@hotmail.com>
2022-06-11 17:34:07 -0300

    Add routes and StepsHeader

--- frontend/front-web/src/App.tsx
+++ frontend/front-web/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Navbar from './Navbar';
+
 import './App.css';
-import Home from'./Home';
+import Routes from'./Routes';
 function App() {
   return (
-    <>
-     <Navbar/>
-     <Home />
-    </>
+    
+     
+     <Routes />
+    
   );
 }
 
