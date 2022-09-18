5e370bb0a8cd1bcf8bcc59fe49f080641a0fd3bc
valmar Silva <valmar-ssilva@hotmail.com>
2022-09-18 16:01:15 -0300

    Create front-web initial files

---
+++ Backend/src/main/java/com/devsuperior/dedeliver/entities/Pproduct.java
@@ -0,0 +1,5 @@
+package com.devsuperior.dedeliver.entities;
+
+public class Pproduct {
+
+}
--- sds2-aulas/front-web/src/Orders/types.ts
+++
@@ -1,9 +0,0 @@
-export type Product = {
-    "id": number;
-    "name": string;
-    "price": number;
-    "description": string;
-    "imageUri": string;
-
-
-}
